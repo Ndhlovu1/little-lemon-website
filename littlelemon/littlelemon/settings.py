facee6c01a9c14f077f8c7631a96016af85622a3
Ndhlovu1 <tinomudaishendhlovu@gmail.com>
2023-02-09 00:30:25 +0200

    Added the Mysql Database Connection

--- littlelemon/littlelemon/settings.py
+++ littlelemon/littlelemon/settings.py
@@ -76,11 +76,21 @@ WSGI_APPLICATION = 'littlelemon.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME' : 'LittleLemonDb',
+        'HOST' : '127.0.0.1',
+        'PORT' : '3306',
+        'USER' : 'djangoUser',
+        'PASSWORD': 'Password@123!', 
+        'OPTIONS': {  
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"  
+            }  
     }
 }
 
+#If you are running mysql -> GRANT ALL ON LittleLemonDb.* TO 'djangoUser'@'localhost';
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
