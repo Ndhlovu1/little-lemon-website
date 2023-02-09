3a6096786e6e18d06883d8c5eb24a949f6e369af
Ndhlovu1 <tinomudaishendhlovu@gmail.com>
2023-02-09 23:42:13 +0200

    The Serializers file with the drf

---
+++ littlelemon/restaurant/serializers.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+from .models import MenuTable, BookingTable
+
+
+
+#Create the Serializer Fields
+class MenuSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MenuTable
+        fields = '__all__'
+
+class BookingTableSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = BookingTable
+        fields = '__all__'
+
+        
+
+
+
+
