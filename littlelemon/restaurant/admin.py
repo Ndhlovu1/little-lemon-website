2fb885792c7d964a29acc950be1c2619a56f9a83
Ndhlovu1 <tinomudaishendhlovu@gmail.com>
2023-02-09 07:03:44 +0200

    Added the models into the admin, to be read in the admin site

--- littlelemon/restaurant/admin.py
+++ littlelemon/restaurant/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import BookingTable, MenuTable
+
+#Add the models so that they may be viewed in the admin site
+admin.site.register(BookingTable)
+admin.site.register(MenuTable)
+
+
+
