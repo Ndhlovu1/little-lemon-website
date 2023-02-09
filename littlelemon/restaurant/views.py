b443d40daa40571a09cd1bbcac8fb1d7863980ae
Ndhlovu1 <tinomudaishendhlovu@gmail.com>
2023-02-09 23:44:27 +0200

    Updated views.py

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,8 +1,24 @@
 from django.shortcuts import render
+from .serializers import MenuSerializer, BookingTableSerializer
+from rest_framework import generics
+from rest_framework.response import Response
+from .models import MenuTable, BookingTable
 
 
 def index(req):
     return render(req, 'index.html', {})
+
+class MenuView(generics.ListCreateAPIView):
+
+    queryset = MenuTable.objects.all()
+    serializer_class = MenuSerializer
+
+
+class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+    queryset = MenuTable.objects.all()
+    serializer_class = MenuSerializer
+
+
 
 
 
