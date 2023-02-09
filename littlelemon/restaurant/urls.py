6b6d0210c5c33fb674a19a8dab7a0c670f1b635f
Ndhlovu1 <tinomudaishendhlovu@gmail.com>
2023-02-09 23:43:13 +0200

    Updated Urls for API Views

--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 urlpatterns = [
     path('',views.index, name='index'),
+    path('menu/', views.MenuView.as_view()),
+    path('menu/<int:pk>', views.SingleMenuItemView.as_view()),
 ]
 
 
