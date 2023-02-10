87583ba2fe0a146afebe16e0347314609ebe3893
Ndhlovu1 <tinomudaishendhlovu@gmail.com>
2023-02-10 08:59:27 +0200

    Added a View Food Items Browsable Api

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -4,22 +4,23 @@ from rest_framework import generics
 from rest_framework.response import Response
 from .models import MenuTable, BookingTable
 from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny, IsAdminUser, IsAuthenticatedOrReadOnly
+from rest_framework.decorators import api_view, permission_classes
+
 
 
 def index(req):
     return render(req, 'index.html', {})
 
+@permission_classes([IsAdminUser])
 class MenuView(generics.ListCreateAPIView):
-
     queryset = MenuTable.objects.all()
     serializer_class = MenuSerializer
 
-
+@permission_classes([IsAuthenticated])
 class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
     queryset = MenuTable.objects.all()
     serializer_class = MenuSerializer
-
 
 
 class BookingViewSet(viewsets.ModelViewSet):
@@ -27,7 +28,10 @@ class BookingViewSet(viewsets.ModelViewSet):
     serializer_class = BookingTableSerializer
     permission_classes = [IsAuthenticated]
 
-
+@permission_classes([AllowAny])
+class MenuItems(generics.ListAPIView,):
+    queryset = MenuTable.objects.all()
+    serializer_class = MenuSerializer
 
 
 
