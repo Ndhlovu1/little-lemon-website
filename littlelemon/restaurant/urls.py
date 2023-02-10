4192c23d2c1aa9451f49dcf66cd390de154503d7
Ndhlovu1 <tinomudaishendhlovu@gmail.com>
2023-02-10 09:00:56 +0200

    Added the food items route

--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 from . import views
+from rest_framework.authtoken.views import obtain_auth_token
 
 
 urlpatterns = [
     path('',views.index, name='index'),
     path('menu/', views.MenuView.as_view()),
     path('menu/<int:pk>', views.SingleMenuItemView.as_view()),
+    path('food/', views.MenuItems.as_view()),
 ]
+
 
 
