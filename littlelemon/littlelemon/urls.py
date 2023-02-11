240fadaae014a2ed7aa7338d158d038c366e27cb
Ndhlovu1 <tinomudaishendhlovu@gmail.com>
2023-02-11 09:06:36 +0200

    Added the djoser url paths

--- littlelemon/littlelemon/urls.py
+++ littlelemon/littlelemon/urls.py
@@ -26,10 +26,29 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('restaurant/', include('restaurant.urls')),
     path('restaurant/booking/', include(router.urls)),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.authtoken'))
 ]
 """
 Super User Details - back-end-guru
 Password : Admin123
+
+
+    /users/
+    /users/me/
+    /users/confirm/
+    /users/resend_activation/
+    /users/set_password/
+    /users/reset_password/
+    /users/reset_password_confirm/
+    /users/set_username/
+    /users/reset_username/
+    /users/reset_username_confirm/
+    /token/login/ (Token Based Authentication)
+    /token/logout/ (Token Based Authentication)
+    /jwt/create/ (JSON Web Token Authentication)
+    /jwt/refresh/ (JSON Web Token Authentication)
+    /jwt/verify/ (JSON Web Token Authentication)
 
 
 """
