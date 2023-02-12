a5831d73a5c4f939820a1d11600f1960434a36e5
Ndhlovu1 <tinomudaishendhlovu@gmail.com>
2023-02-12 23:21:30 +0200

    Read Comments

--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -1,14 +1,13 @@
 from django.urls import path
 from . import views
-from rest_framework.authtoken.views import obtain_auth_token
-
+from rest_framework.authtoken.views import ObtainAuthToken
 
 urlpatterns = [
     path('',views.index, name='index'),
     path('menu/', views.MenuView.as_view()),
     path('menu/<int:pk>', views.SingleMenuItemView.as_view()),
     path('food/', views.MenuItems.as_view()),
-    path('api-token-auth/', obtain_auth_token),
+    path('api-token-auth/', ObtainAuthToken.as_view()),
 
 ]
 
